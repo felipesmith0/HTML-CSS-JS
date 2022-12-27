8ab4b38bb0d72e858bd777f72570c20c3cabb07c
felipesmith0 <felipeclasher041@gmail.com>
2022-12-27 12:53:10 -0300

    primeira att

---
+++ HTML-CSS- JS/atividades/JS/aula 10 e 11/aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 50.5
+console.log(`a velocidade do carro é ${vel}Km/h `)
+if(vel >50) { //CONDIÇÃO SIMPLES , SÓ IRÁ EXECUTAR O BLOCO SE FOR TRUE , VELOCIDADE ACIMA DE 50KM/H
+    console.log(`Você ultrapasssou o limite de velocidade . MULTADO! `)
+}
+
+console.log(` Dirija sempre usando o cinto de segurança !`)
---
+++ HTML-CSS- JS/atividades/JS/aula 10 e 11/aula11/ex009.js
@@ -0,0 +1,10 @@
+//CONDIÇÃO COMPOSTA . TRUE OU FALSE
+
+var país = 'ARGENTINA'
+console.log(` Vivendo em ${país}`)
+if( país == 'BRASIL'){ 
+    console.log(`Você é Brasileiro!`)
+}else{
+console.log(`Você é Estrangeiro!`)
+
+}
